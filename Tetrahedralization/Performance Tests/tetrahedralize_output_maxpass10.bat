dfa348f55d41506a21fa697a6854fe9c9856e43c
Tom <tom.ille@gmx.de>
2020-03-17 20:57:30 +0100

    Performance Test Folder

---
+++ Tetrahedralization/Performance Tests/tetrahedralize_output_maxpass10.bat
@@ -0,0 +1,3 @@
+SET /p IFile=Enter Input File name:
+::SET /p OFile=Enter Output File name:
+F:\Programme\TetWild\build\Debug\TetWild --input "%cd%\%IFile%" --output "%cd%\%IFile%.mesh" --max-pass 10
